2a1516bf36decaee2bfb7e36606fe2e3be06dd35
siobhan <sr@saibhreas.com>
2021-06-11 20:52:10 -0400

    This is the BEGINNING!

---
+++ models/Note.js
@@ -0,0 +1,35 @@
+const { stringify } = require("querystring");
+const { UUIDV4, Model, DataTypes, condition } = require("sequelize");
+const sequelize = require("../config/connection");
+class Note extends Model {}
+
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+
+    condition: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Note",
+  }
+);
+module.exports = Note;
