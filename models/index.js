84adc2a9d7c0acc6c65d21c851b43f490972dd65
siobhan <sr@saibhreas.com>
2021-06-14 15:02:47 -0400

    added passport. Still refactoring

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const User = require('./user');
+const Note = require('./note');
+const Log = require('./log');
+const Park = require("./park")
+
+User.hasMany(Log, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Note, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Park, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Log.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Park.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Note.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+
+module.exports = { User, Post, Favorite };
+
