046d55124519a9671ada083b86180ff0b9e542cd
siobhan <sr@saibhreas.com>
2021-06-15 10:47:41 -0400

    made changes seperating pages and arranging routes

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
