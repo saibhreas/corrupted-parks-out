d0c314a04fe7212652f15f3ae4de4b69716e3dea
siobhan <sr@saibhreas.com>
2021-06-16 08:33:37 -0400

    creted wrok flow for next 2 days. fixed links in html

---
+++ controller/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./v1');
+
+router.use('/v1',apiRoutes);
+
+module.exports= router;
--- controller/api/v1/userAuth.js
+++ controller/api/v1/userAuth.js
@@ -4,6 +4,7 @@ const db = require("../models");
 const passport = require("./config/passport");
 
 router.post("/signup", function (req, res) {
+  console.log("POST /api/v1/auth/signup");
   db.User.findOne({ email: req.body.email }, (err, user) => {
     if (err) {
       console.log(err);
@@ -11,17 +12,20 @@ router.post("/signup", function (req, res) {
       res.json({ msg: "This user already has an account!" });
     } else {
       db.User.create(req.body).then(function () {
-        res.redirect(307, "/login");//redirect to log in to be authenticated
+        res.redirect(307, "/login"); //redirect to log in to be authenticated
       });
     }
   });
 });
-router.post("/login", passport.authenticate("local"), function (req, res) {
-  console.log("login hit");
+
+router.post("/login",/* passport.authenticate("local"),*/ function (req, res) {
+  console.log("POST /api/v1/auth/login");
+
   res.json(req.user);
 });
+
 router.get("/logout", function (req, res) {
-  console.log("logout!");
+  console.log("POST /api/v1/auth/logout");
   req.logout();
   res.sendStatus(200);
 });
