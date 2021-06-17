d2e95d33313663c33ff5b478ff01cda6b1339da1
C. Rando <crando@gmail.com>
2021-06-17 13:38:20 -0400

    midday push to get everyone linked

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,25 @@
 DROP DATABASE IF EXISTS parkout_db;
 CREATE DATABASE parkout_db;
-USE parkout_db;
+-- USE parkout_db;
 	
 
-CREATE TABLE Users (
-  id INT AUTO_INCREMENT NOT NULL,
-  user_name VARCHAR (30) NOT NULL,
-  user_email VARCHAR (60) NOT NULL,
-  user_password VARCHAR (5) NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE Users (
+--   id INT AUTO_INCREMENT NOT NULL,
+--   user_name VARCHAR (30) NOT NULL,
+--   user_email VARCHAR (60) NOT NULL,
+--   user_password VARCHAR (5) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
--- removed admissions
-CREATE TABLE Parks (
-  id INT AUTO_INCREMENT NOT NULL,
-  park_name VARCHAR (60) NOT NULL,
-  park_phone_number INTEGER,
-  park_activities VARCHAR (60) NULL,
-  park_fees INTEGER,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
-);            
+-- -- removed admissions
+-- CREATE TABLE Parks (
+--   id INT AUTO_INCREMENT NOT NULL,
+--   user_id INTEGER NOT NULL,
+--   park_name VARCHAR (60) NOT NULL,
+--   park_phone_number INTEGER,
+--   park_activities VARCHAR (60) NULL,
+--   park_fees INTEGER,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
+-- );            
 
